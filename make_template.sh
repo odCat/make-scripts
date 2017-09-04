98827888ca5030a737daeb2846e425c6f91db767
Mihai Gătejescu <mgatejescu@contractor.ea.com>
2017-09-04 18:54:22 +0300

    add license

--- make_template.sh
+++ make_template.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
-#===================================================
+#===============================================================================
 #
-#        FILE: btmp.sh
+#	   FILE: make_template.sh
 #
-# DESCRIPTION: Create Java source code templete file
+#   DESCRIPTION: Create Java source code templete file
 #
-#      AUTHOR: Mihai Gătejescu
-#     VERSION: 1.3.4
-#     CREATED: 15.08.2017
-#===================================================
+#	 AUTHOR: Mihai Gătejescu
+#	VERSION: 1.3.5
+#	CREATED: 15.08.2017
+#
+#===============================================================================
+
+#===============================================================================
+#
+#	Copyright [yyyy] [name of copyright owner]
+#
+#	Licensed under the Apache License, Version 2.0 (the "License");
+#	you may not use this file except in compliance with the License.
+#	You may obtain a copy of the License at
+#
+#	    http://www.apache.org/licenses/LICENSE-2.0
+#
+#	Unless required by applicable law or agreed to in writing, software
+#	distributed under the License is distributed on an "AS IS" BASIS,
+#	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#	See the License for the specific language governing permissions and
+#	limitations under the License.
+#
+#===============================================================================
 
 # Display usage and exit, if erroneous input
 if [ ! $# == 1 ]; then
