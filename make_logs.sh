5cac6e2acf1cf04401451e961605e411481ebcf6
Mihai Gătejescu <gus666xe@gmail.com.com>
2017-09-06 00:44:05 +0300

    Adding the logs script

---
+++ make_logs.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#==========================================================================
+#        FILE: make_logs.sh
+#
+# DESCRIPTION: Pipes the output of the adb logcat command to the tee 
+#              command to write to stdout and to write logfiles
+#              Creates a directory tree: year/month/daylogfile.
+#
+#      AUTHOR: Mihai Gătejescu
+#     VERSION: 0.0.1
+#     CREATED: 05.09.2017
+#==========================================================================
+
+#==========================================================================
+# Copyright 2017 Mihai Gătejescu
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#	http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#==========================================================================
+
+
+
