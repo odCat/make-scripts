cc82888c94b1f0daf0629577c782f633d4160075
Mihai Gătejescu <gus666xe@gmail.com>
2019-04-10 22:45:55 +0300

    Add make-info.sh
    INCOMPLETE
    Returns information about a connected Andorid device.

---
+++ make-info.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#==========================================================================
+#        FILE: make-info.sh
+#
+# DESCRIPTION: Display Android device informations
+#
+#      AUTHOR: Mihai Gătejescu
+#     VERSION: 1.0.0
+#     CREATED: 10.04.2019
+#==========================================================================
+
+#==========================================================================
+# Copyright 2019 Mihai Gătejescu
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#	http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#==========================================================================
+
+deviceid=$(adb devices | grep -v -e "List" | cut -f 1 -d '      ')
+
+adb -s $deviceid shell getprop
