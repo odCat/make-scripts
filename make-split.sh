233d54c9ed1b3e9b370755eca9e3e5109132b709
Mihai Gătejescu <gus666xe@gmail.com>
2019-07-03 19:56:20 +0300

    Add copyright and license notices to split

---
+++ make-split.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#==========================================================================
+#        FILE: make_split.sh
+#
+# DESCRIPTION: Splits one file into multiple files
+#              The default number is two
+#
+#      AUTHOR: Mihai Gătejescu
+#     VERSION: 1.0.0
+#     CREATED: 03.07.2019
+#==========================================================================
+
+#==========================================================================
+# Copyright 2017 Mihai Gătejescu
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#	http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#==========================================================================
