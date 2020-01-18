0d8085953909b66928a7286ece20723182c54f6d
Mihai Gătejescu <gus666xe@gmail.com>
2020-01-18 15:35:18 +0200

    Update copyright years

--- make-logs.sh
+++ make-logs.sh
@@ -12,7 +12,7 @@
 #==========================================================================
 
 #==========================================================================
-# Copyright 2017 Mihai Gătejescu
+# Copyright 2017, 2018, 2019, 2020 Mihai Gătejescu
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
